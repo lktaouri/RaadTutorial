01e48ca4227443fbd6662ec833b64e2de98ee034
lktaouri <mohammedlaktaoui@outlook.it>
2022-06-21 18:54:58 +0200

    Commie raad

--- src/main/java/com/example/raadtutorial/RaadController.java
+++ src/main/java/com/example/raadtutorial/RaadController.java
@@ -1,7 +1,12 @@
 package com.example.raadtutorial;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RaadController {
+    @RequestMapping("/Hello")
+    public String hello(){
+        return "Hello";
+    }
 }
